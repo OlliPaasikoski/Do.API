b6e317dcc1a498f2f467be1a1db391ea28b2d408
OlliPaasikoski <olli.paasikoski@gmail.com>
2017-05-30 22:57:36 +0300

    basic bootstrapping

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Do.API.Controllers
+{
+    [Route("api/tasks")]
+    public class TasksController : Controller
+    {
+        // GET api/tasks
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "task1", "task2" };
+        }
+
+        // GET api/tasks/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "task5";
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -29,13 +29,23 @@ namespace Do.API
         {
             // Add framework services.
             services.AddMvc();
+
+            var connectionString = Configuration["connectionStrings:doDBConnectionString"];
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler();
+            }
 
             app.UseMvc();
         }
